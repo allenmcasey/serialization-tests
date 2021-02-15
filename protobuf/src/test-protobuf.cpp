c0b62e90e26222c89f3e9214eb5607ec63f5a5b4
Allen Casey <36112600+allenmcasey@users.noreply.github.com>
2021-02-15 10:50:53 -0600

    refactor to test new TestObject implementation

--- protobuf/src/test-protobuf.cpp
+++ protobuf/src/test-protobuf.cpp
@@ -1,12 +1,38 @@
-// Simple test of the serialization of
-// a Sensor object
-
-#include "sensor.pb.h"
+#include "TestObject.pb.h"
+#include <iostream>
+#include <chrono> 
 
 int main() {
-    Sensor sensor;
-    sensor.set_name("Laboratory");
-    sensor.set_temperature(23.4);
-    sensor.set_humidity(68);
-    sensor.set_door(Sensor_SwitchLevel_OPEN);
+    
+    std::vector<int> intVect{1, 2, 3, 4, 5};
+    std::vector<std::string> strVect{"str1", "str2", "str3", "str4", "str5"};
+    std::string data;
+
+    // create object
+    auto t1 = std::chrono::high_resolution_clock::now();
+    TestObject testObject;
+    testObject.set_int1(1);
+    testObject.set_int2(2);
+    testObject.set_int3(3);
+    testObject.set_int4(4);
+    testObject.set_s1("string 1");
+    testObject.set_s2("string 2");
+    testObject.set_s3("string 3");
+    testObject.set_s4("string 4");
+    *testObject.mutable_strings() = {strVect.begin(), strVect.end()}; 
+    *testObject.mutable_ints() = {intVect.begin(), intVect.end()};
+
+    // serialize object
+    testObject.SerializeToString(&data);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    printf("\nObject packed successfully... Time: %lld ms\n", duration);
+    
+    // deserialize previous object into new object
+    t1 = std::chrono::high_resolution_clock::now();
+    TestObject testObject2;
+    testObject2.ParseFromString(data);
+    t2 = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+    printf("Object unpacked successfully... Time: %lld ms\n\n", duration);
 }
